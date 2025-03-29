c4bf01732d2e99a08a3913d4baa1762409d392d8
Potatang <R12323028@ntu.edu.tw>
2025-03-29 19:57:35 +0800

    3/29

--- moralcost/__init__.py
+++ moralcost/__init__.py
@@ -7,9 +7,25 @@ Your app description
 
 
 class C(BaseConstants):
-    NAME_IN_URL = 'moralcost'
-    PLAYERS_PER_GROUP = None
+    NAME_IN_URL = 'moral cost'
+    PLAYERS_PER_GROUP = 2
     NUM_ROUNDS = 1
+
+    ADVISOR_ROLE = '推薦人'
+    CLIENT_ROLE = '客戶'
+
+    # Product A: 抽到 $2 的機率
+    PRODUCT_A_SUCCESS_PROB = 0.6
+    # Product B: 依據品質狀態決定抽到 $2 的機率
+    PRODUCT_B_SUCCESS_PROB_H = 0.8
+    PRODUCT_B_SUCCESS_PROB_L = 0.4
+    # wage
+    WAGE = 15
+    # commission price
+    COMMISSION = 5
+    # 球的價值
+    GOODBALL = 65
+    BADBALL = 0   
 
 
 class Subsession(BaseSubsession):
