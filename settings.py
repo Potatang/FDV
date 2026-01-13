221d710b3aeb246db7bacd2c291358d8d7482009
Potatang <R12323028@ntu.edu.tw>
2026-01-13 13:41:36 +0900

    1/13

--- settings.py
+++ settings.py
@@ -4,13 +4,13 @@ from os import environ
 SESSION_CONFIGS = [
     dict(
         name='FDI',
-        num_demo_participants=4,   
+        num_demo_participants=2,   
         app_sequence=[
             'start_app',
             'moralcost_IF',
-            # 'experiment_IF',
-            # 'choice',
-            # 'guessquality_IF',
+            'experiment_IF',
+            'choice',
+            'guessquality_IF',
             'moralcost2',
             'guessproportion_IF',
             'realizemoralcost',
